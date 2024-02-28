3a47d1edd314d5c76e14a42e911b7c5e7147e795
Jonny Condor <jonnathancondor90@gmail.com>
2024-02-27 19:30:20 -0500

    Activelink updated

--- components/ActiveLink.tsx
+++ components/ActiveLink.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { CSSProperties } from "react";
 
 interface ActiveLinkProps {
     text: string;
     href: string;
 }
 
-const style= {
+const style: CSSProperties= {
  color:'#0070f3',
  textDecoration:'underline',
 }
@@ -16,7 +17,7 @@ export const ActiveLink = ({text, href}:ActiveLinkProps) => {
     const { asPath } = useRouter();
 
   return (
-    <Link href={href} style={ asPath === href ? style : null }>
+    <Link href={href} style={ asPath === href ? style : undefined }>
         {text}       
     </Link>
   )
